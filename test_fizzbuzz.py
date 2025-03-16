ebf0fe34aa5de7f1848529eb458dcaa3342a24f0
srob <sjroberts@gmail.com>
2025-03-16 20:47:30 +0000

    Corrected ci.yml for running python unit tests

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,8 +1,18 @@
 import unittest
+from fizzbuzz import fizzbuzz
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
+
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+
+    def test_number(self):
+        self.assertEqual(fizzbuzz(7), 7)
 
 if __name__ == '__main__':
     unittest.main()
